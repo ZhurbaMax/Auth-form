19ab66fdf3f99666e31e5667a23821d066f96e13
ZhurbaMax <maksimjurba90210@gmail.com>
2021-01-06 15:19:05 +0200

    3 commit

---
+++ views/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title> Registration // Authorization</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/register.php">Register</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/auth.php">Authorization</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+</body>
+</html>
